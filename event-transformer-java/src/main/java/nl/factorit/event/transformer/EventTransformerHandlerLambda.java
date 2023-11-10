bf9572ce470da966947f74b188f507b9df16f6e6
Thomas <thomas.heinsbroek@factorit-external.nl>
2023-11-10 11:06:15 +0100

    prettier

--- event-transformer-java/src/main/java/nl/factorit/event/transformer/EventTransformerHandlerLambda.java
+++ event-transformer-java/src/main/java/nl/factorit/event/transformer/EventTransformerHandlerLambda.java
@@ -2,21 +2,17 @@ package nl.factorit.event.transformer;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.util.stream.IntStream;
-
 @ApplicationScoped
 public class EventTransformerHandlerLambda implements RequestHandler<Object, String> {
 
     @Override
     public String handleRequest(Object event, Context context) {
-        System.out.println("test 123!");
+        //The JSON object is parsed by Java to a LinkedHashMap
+        System.out.println(event);
+
+        String coolVariable = System.getenv("cool");
 
         return null;
     }
--- event-transformer-ts/lambda.ts
+++ event-transformer-ts/lambda.ts
@@ -5,17 +5,13 @@ const dynamo = new DynamoDB.DocumentClient();
 const TABLE_NAME : string = process.env.HELLO_TABLE_NAME!;
 
 export const handler: Handler = async (event, context) => {
-
-    console.log('before break')
-
+    //This is an example Lambda which does things dependent on the given context of the event
     const method = event.requestContext.http.method;
 
-    console.log('after break')
-
     if (method === 'GET') {
-        return await getHello(event)
+        return 'Method was GET'
     } else if (method === 'POST') {
-        return await save(event);
+        return 'Method was POST'
     } else {
         return {
             statusCode: 400,
@@ -23,80 +19,3 @@ export const handler: Handler = async (event, context) => {
         };
     }
 };
-
-async function save(event : any) {
-    const name = event.queryStringParameters.name;
-
-    const item = {
-        name: name,
-        date: Date.now(),
-    };
-
-    console.log(item);
-    const savedItem = await saveItem(item);
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(savedItem),
-    };
-};
-
-async function getHello(event : any ) {
-    const name = event.queryStringParameters.name;
-
-    const item = await getItem(name);
-
-    if (item !== undefined && item.date) {
-        const d = new Date(item.date);
-
-        const message = `Was greeted on ${d.getDate()}/${
-            d.getMonth() + 1
-        }/${d.getFullYear()}`;
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(message),
-        };
-
-    } else {
-
-        const message = "Nobody was greeted with that name";
-        return {
-            statusCode: 200,
-            body: JSON.stringify(message),
-        };
-    }
-};
-
-async function getItem(name : string ) {
-
-    const params : DynamoDB.DocumentClient.GetItemInput = {
-        Key: {
-            name: name,
-        },
-        TableName: TABLE_NAME,
-    };
-
-    return dynamo
-        .get(params)
-        .promise()
-        .then((result) => {
-            console.log(result);
-            return result.Item;
-        });
-}
-
-async function saveItem(item : any) {
-
-    const params : DynamoDB.DocumentClient.PutItemInput = {
-        TableName: TABLE_NAME,
-        Item: item,
-    };
-
-    return dynamo
-        .put(params)
-        .promise()
-        .then(() => {
-            return item;
-        });
-}
